2094297f62dc2cd94d3fb5f3b8ae16fe32fa0e97
David Gundersen <gundyman@gmail.com>
2015-11-26 00:22:45 +1100

    Get systemJS working in dev mode

--- system.config.js
+++ system.config.js
@@ -8,8 +8,8 @@ System.config({
     ]
   },
   "paths": {
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js",
+    "github:*": "../jspm_packages/github/*.js",
+    "npm:*": "../jspm_packages/npm/*.js",
     "*": "*.js"
   },
   "defaultJSExtensions": true,
